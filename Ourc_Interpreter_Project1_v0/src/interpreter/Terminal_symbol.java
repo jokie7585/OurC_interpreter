b839effaf97ecdfc89614a8b3f5c18f149a2b85c
jokie7585 <35581875+jokie7585@users.noreply.github.com>
2020-03-30 14:19:28 +0800

    enum->class

--- Ourc_Interpreter_Project1_v0/src/interpreter/Terminal_symbol.java
+++ Ourc_Interpreter_Project1_v0/src/interpreter/Terminal_symbol.java
@@ -1,13 +1,7 @@
 package interpreter;
 
 public class Terminal_symbol {
-	public static final Terminal_symbol IDENT;
-    public static final Terminal_symbol NUM;
-    public static final Terminal_symbol Delimiter;
-    
-    static {
-    	IDENT = new Terminal_symbol();
-    	NUM = new Terminal_symbol();
-    	Delimiter = new Terminal_symbol();
-    }
+	public static final Terminal_symbol IDENT = new Terminal_symbol();
+    public static final Terminal_symbol NUM = new Terminal_symbol();
+    public static final Terminal_symbol Delimiter = new Terminal_symbol();
 }
