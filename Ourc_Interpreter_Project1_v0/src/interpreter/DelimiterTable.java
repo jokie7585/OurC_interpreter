b934f033d100eb96c65674352be4e5d5af8536d9
jokie7585 <35581875+jokie7585@users.noreply.github.com>
2020-03-30 14:12:32 +0800

    RF:foreach->for

--- Ourc_Interpreter_Project1_v0/src/interpreter/DelimiterTable.java
+++ Ourc_Interpreter_Project1_v0/src/interpreter/DelimiterTable.java
@@ -1,21 +1,21 @@
 package interpreter;
 
 public class DelimiterTable {
-  public static char[] delimiter = { ';', '(', ')', '+', '-', '*', '/', '>', '<', '=', '\"', '\'', '$', '%', '@', '#',
-      '&', '^', '-', '!' };
+  public static char[] delimiter = { ';', '(', ')', '+', '-', '*', '/', '>', '<', '=', '\"', '\'',
+		  '$', '%', '@', '#', '&', '^', '-', '!' };
   public static char[] enabled_delimiter = { ';', '(', ')', '+', '-', '*', '/', '>', '<', '=', '\"', '\'' };
   
   public static boolean is_delimiter( char character ) {
-    for ( char c : delimiter ) {
-      if ( c == character )
+    for ( int i = 0 ; i < delimiter.length ; i++ ) {
+      if ( delimiter[i] == character )
         return true;
     }
     return false;
   }
   
   public static boolean is_enabled_Delimiter( char character ) {
-    for ( char element : enabled_delimiter ) {
-      if ( element == character )
+    for (  int i = 0 ; i < delimiter.length ; i++ ) {
+      if ( enabled_delimiter[i] == character )
         return true;
     }
     
